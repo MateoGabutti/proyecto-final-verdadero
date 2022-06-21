71a9d4cf65f84ce9170cbf703917b3b38ac9aa73
Mateo Gabutti <mateogabutti12@gmail.com>
2022-06-21 13:59:53 -0300

    if / else / else if

---
+++ codigo.js
@@ -0,0 +1,96 @@
+let desayuno = prompt("Que soles tomar en el desayuno?").toLowerCase()
+let desayunoComida = prompt("Que soles comer en el desayuno?").toLowerCase()
+
+if(desayuno == "cafe"){
+    alert("Desayunas cafe, que rico")
+    if(desayunoComida == "tostadas"){
+        alert( desayuno + " con tostadas, que rico!")
+    }else if(desayunoComida == "galletitas"){
+        alert( desayuno + " con galletitas, que rico!")
+    }else if(desayunoComida == "medialunas"){
+        alert( desayuno + " con medialunas, que rico!")
+    } else if(desayunoComida == "torta"){
+        alert( desayuno + " con torta, que rico!")
+    }else if(desayunoComida == "bizcochitos"){
+        alert( desayuno + " con bizcochitos, que rico!")
+    }else{
+        alert( desayuno + " y no comes nada")
+    }
+}else if(desayuno == "mate"){
+    alert("Desayunas mate, que rico")
+    if(desayunoComida == "tostadas"){
+        alert( desayuno + " con tostadas, que rico!")
+    }else if(desayunoComida == "galletitas"){
+        alert( desayuno + " con galletitas, que rico!")
+    }else if(desayunoComida == "medialunas"){
+        alert( desayuno + " con medialunas, que rico!")
+    } else if(desayunoComida == "torta"){
+        alert( desayuno + " con torta, que rico!")
+    }else if(desayunoComida == "bizcochitos"){
+        alert( desayuno + " con bizcochitos, que rico!")
+    }else{
+        alert( desayuno + " y no comes nada")
+    }
+}else if(desayuno == "te"){
+    alert("Desayunas te, que rico")
+    if(desayunoComida == "tostadas"){
+        alert( desayuno + " con tostadas, que rico!")
+    }else if(desayunoComida == "galletitas"){
+        alert( desayuno + " con galletitas, que rico!")
+    }else if(desayunoComida == "medialunas"){
+        alert( desayuno + " con medialunas, que rico!")
+    } else if(desayunoComida == "torta"){
+        alert( desayuno + " con torta, que rico!")
+    }else if(desayunoComida == "bizcochitos"){
+        alert( desayuno + " con bizcochitos, que rico!")
+    }else{
+        alert( desayuno + " y no comes nada")
+    }
+}else if(desayuno == "chocolatada"){
+    alert("Desayunas chocolatada, que rico")
+    if(desayunoComida == "tostadas"){
+        alert( desayuno + " con tostadas, que rico!")
+    }else if(desayunoComida == "galletitas"){
+        alert( desayuno + " con galletitas, que rico!")
+    }else if(desayunoComida == "medialunas"){
+        alert( desayuno + " con medialunas, que rico!")
+    } else if(desayunoComida == "torta"){
+        alert( desayuno + " con torta, que rico!")
+    }else if(desayunoComida == "bizcochitos"){
+        alert( desayuno + " con bizcochitos, que rico!")
+    }else{
+        alert( desayuno + " y no comes nada")
+    }
+}else if(desayuno == "agua"){
+    alert("Desayunas agua, que rico")
+    if(desayunoComida == "tostadas"){
+        alert( desayuno + " con tostadas, que rico!")
+    }else if(desayunoComida == "galletitas"){
+        alert( desayuno + " con galletitas, que rico!")
+    }else if(desayunoComida == "medialunas"){
+        alert( desayuno + " con medialunas, que rico!")
+    } else if(desayunoComida == "torta"){
+        alert( desayuno + " con torta, que rico!")
+    }else if(desayunoComida == "bizcochitos"){
+        alert( desayuno + " con bizcochitos, que rico!")
+    }else{
+        alert( desayuno + " y no comes nada")
+    }
+}else if(desayuno == "nesquik"){
+    alert("Desayunas nesquik, que rico")
+    if(desayunoComida == "tostadas"){
+        alert( desayuno + " con tostadas, que rico!")
+    }else if(desayunoComida == "galletitas"){
+        alert( desayuno + " con galletitas, que rico!")
+    }else if(desayunoComida == "medialunas"){
+        alert( desayuno + " con medialunas, que rico!")
+    } else if(desayunoComida == "torta"){
+        alert( desayuno + " con torta, que rico!")
+    }else if(desayunoComida == "bizcochitos"){
+        alert( desayuno + " con bizcochitos, que rico!")
+    }else{
+        alert( desayuno + " y no comes nada")
+    }
+}else{
+    alert("no desayunas")
+}
